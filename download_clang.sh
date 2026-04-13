39b645280434fbbc50cceb046f67b98784777186
momo-trip <momoko.shiraishi36@gmail.com>
2026-04-13 10:39:33 -0500

    updated

--- download_clang.sh
+++ download_clang.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-#git clone --depth 1 --branch llvmorg-19.1.7 https://github.com/llvm/llvm-project.git
+git clone --depth 1 --branch llvmorg-19.1.7 https://github.com/llvm/llvm-project.git
 
 cd llvm-project
 git apply ../clang-modifications.patch
 
 # Build
+# rm -rf build
 cmake -S llvm -B build -G Ninja \
   -DLLVM_ENABLE_PROJECTS=clang \
+  -DLLVM_TARGETS_TO_BUILD="X86;AArch64" \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_INSTALL_PREFIX=../llvm-custom
-cmake --build build
+cmake --build build --parallel 2
 cmake --install build
-
-# git apply ../clang-modifications.patch
